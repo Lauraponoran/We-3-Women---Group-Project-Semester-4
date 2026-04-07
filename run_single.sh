33566388a0c21f632bc19dccb4888e90a2c6faaa
mdscolour <likunhede@gmail.com>
2026-04-07 11:12:35 +0200

    requirement

--- run_single.sh
+++ run_single.sh
@@ -2,7 +2,7 @@
 
 # ============================
 # How to submit:
-# sbatch run_array.sh
+# sbatch run_single.sh
 #
 # Check job:
 # squeue -u scur0741
@@ -20,8 +20,8 @@
 #SBATCH --job-name=publisher1
 #SBATCH --output=/home/scur0741/kun/logs/%j.out
 #SBATCH --error=/home/scur0741/kun/logs/%j.err
-#SBATCH --time=200:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --time=90:00:00
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 
 cd /home/scur0741/kun/We-3-Women---Group-Project-Semester-4
@@ -31,6 +31,6 @@ source /home/scur0741/kun/venv-agent/bin/activate
 echo "Running on $(hostname)"
 echo "Start time: $(date)"
 
-python collect_articles.py --workers 4
+python collect_articles.py
 
 echo "End time: $(date)"
